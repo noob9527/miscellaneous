5102df0dbf51e5611ca22e6e6c9679839e42e51b
noob9527 <2061864@qq.com>
2020-03-03 01:45:18 +0800

    add script

---
+++ git/change-commit-name.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# https://help.github.com/en/github/using-git/changing-author-info
+# https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="mxu@capvision.com"
+CORRECT_NAME="noob9527"
+CORRECT_EMAIL="2061864@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
